089866ad4c77a1e7f8bc20580a34e410dee0a045
Wladimir Guevara <wladige@gmail.com>
2020-08-08 20:20:46 -0400

    estilos y modificacion del DOM terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Declaracion de los import 
 import circulo from './Circulo.js'
 import poligonos from './Poligono.js'
 import decagono from './Decagono.js'
@@ -5,34 +6,35 @@ import octagono from './Octagono.js'
 import triangulo from './Triangulo.js'
 import cuadrado from './cuadrado.js'
 
-
-
-
+// Funcion detectora de eventos y sus condicionales
 document.getElementsByTagName('select')[0].addEventListener('change', function (e) {
   console.log(poligonos.getValues())
   let poligono = e.target.value
+  let paragraph = document.getElementById("texto");
+  let paragraph2 = document.getElementById("texto2");
   if (poligono === 'circulo') {
-    console.log(`Perímetro del Círculo ===> ${circulo.perimetro()}`)
-    console.log(`Área del Círculo ===> ${circulo.area()}`)
+    paragraph.textContent  = (`El Perímetro del Círculo es: ${circulo.perimetro()}`)
+    paragraph2.textContent = (`El Área del Círculo es:  ${circulo.area()}`)
   }
   if (poligono === 'cuadrado') {
 
-    console.log(`Perímetro del Cuadrado ===> ${cuadrado.perimetro()}`)
-    console.log(`Área del Cuadrado ===> ${cuadrado.area()}`)
+    paragraph.textContent = (`El Perímetro del Cuadrado es:  ${cuadrado.perimetro()}`)
+    paragraph2.textContent= (`El Área del Cuadrado es: ${cuadrado.area()}`)
   }
   if (poligono === 'triangulo') {
 
-    console.log(`Perímetro del Triángulo ===> ${triangulo.perimetro()}`)
-    console.log(`Área del Triángulo ===> ${triangulo.area()}`)
+    paragraph.textContent = (`El Perímetro del Triangulo es: ${triangulo.perimetro()}`)
+    paragraph2.textContent = (`El Área del Triángulo es:  ${triangulo.area()}`)
   }
   if (poligono === 'octagono') {
 
-    console.log(`Perímetro del Octágono ===> ${octagono.perimetro()}`)
-    console.log(`Área del Octágono ===> ${octagono.area()}`)
+    paragraph.textContent = (`El Perímetro del Octagono es:${octagono.perimetro()}`)
+    paragraph2.textContent = (`El Área del Octágono es: ${octagono.area()}`)
   }
   if (poligono === 'decagono') {
 
-    console.log(`Perímetro del Decágono ===> ${decagono.perimetro()}`)
-    console.log(`Área del Decágono ===> ${decagono.area()}`)
+    paragraph.textContent = (`El Perímetro del Decagono es: ${decagono.perimetro()}`)
+    paragraph2.textContent = (`El Área del Decágonoes: ${decagono.area()}`)
   }
 })
+
